6c7dad550ae4f48b5dbbf39bf6c64bc206a7df2d
CNC5 <kokfbbat@mail.ru>
2025-07-28 22:34:04 +0300

    fix: Create missing directories when downloading binaries

--- download_binaries.sh
+++ download_binaries.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
+mkdir -p serverops/bin
 curl -o serverops/bin/nginx https://file.cnc5.dev/nginx/1.27.5/nginx
 curl -o serverops/bin/sing-box https://file.cnc5.dev/sing-box/1.12.0/sing-box
