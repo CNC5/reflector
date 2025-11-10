3188133a7b7b36a9637eea34c44826833ba8d30d
CNC5 <github@cnc5.dev>
2025-11-10 03:28:09 +0300

    [feat]: cobra cli initial setup

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"os"
+	"reflector/log"
+
+	"github.com/spf13/cobra"
+)
+
+var debug *bool
+var reflectorConfigLocation *string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "reflector",
+	Short: "",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if *debug {
+			log.SetDefaultLogger(log.NewLogger("reflector", log.DEBUG))
+		} else {
+			log.SetDefaultLogger(log.NewLogger("reflector", log.INFO))
+		}
+		log.GetDefaultLogger().Debug().Msg("Debugging enabled")
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	debug = rootCmd.PersistentFlags().BoolP("debug", "d", false, "Enable debugging")
+	reflectorConfigLocation =
+		rootCmd.PersistentFlags().StringP(
+			"reflector-config", "r",
+			"./config.yaml",
+			"Reflector config location",
+		)
+}
